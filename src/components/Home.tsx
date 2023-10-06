116cf7d6fea1ed9f0ec437baf81e05f0b661378f
RageNeko26 <rneko2006@gmail.com>
2023-10-06 23:52:55 +0700

    feat: Add Forgot-Password API component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,4 @@
-import { Container, Row, Col } from "react-bootstrap"
+import { Container, Row, Col, Badge } from "react-bootstrap"
 
 export default function Home() {
   return(
@@ -9,6 +9,14 @@ export default function Home() {
             <h3 className="fs-4">API Reference for CVZN</h3>
               <p className="text-secondary">Sebuah dokumentasi dan sample code API Web Services CV Zaman Now. Diperuntukan untuk developer internal, baik Front-End Engineer maupun Back-end Engineer.</p>
     </div>
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        <div className="my-3">
+          <h3 className="fs-4">List API</h3>
+          <p className="fs-5"><Badge>Forgot Password</Badge> </p> 
+        </div>
       </Col>
     </Row>
   </Container>
