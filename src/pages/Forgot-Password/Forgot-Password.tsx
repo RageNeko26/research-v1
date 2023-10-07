9f94a625505d37fa2fcda401edd46cbff5f2bde7
RageNeko26 <rneko2006@gmail.com>
2023-10-07 19:40:22 +0700

    chore: Add API Description

--- src/pages/Forgot-Password/Forgot-Password.tsx
+++ src/pages/Forgot-Password/Forgot-Password.tsx
@@ -1,8 +1,15 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+
 export default function ForgotPassword() {
   return(
-      <>
-        <h2>Forgot Password</h2>
-        <p>Fitur ini bekerja dengan menggunakan Redis sebagai teknologi Cache.</p>
-      </>
+      <Container className="my-3">
+       <Row lg={2}>
+        <Col>
+          <h2>Forgot Password</h2>
+          <p className="text-secondary">Fitur ini bekerja dengan menggunakan Redis sebagai teknologi Cache. Dengan adanya cache, kita bisa melakukan tranksaksi data dengan lebih cepat.</p>
+        </Col>
+       </Row>
+      </Container>
     )
 } 
