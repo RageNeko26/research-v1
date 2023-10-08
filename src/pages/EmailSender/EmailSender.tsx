0eb892f222cb009dea8d0bb151d540b04162275b
RageNeko26 <rneko2006@gmail.com>
2023-10-08 18:44:31 +0700

    chore: Add overview description

--- src/pages/EmailSender/EmailSender.tsx
+++ src/pages/EmailSender/EmailSender.tsx
@@ -6,6 +6,7 @@ export default function EmailSender() {
           <Row lg={2}>
             <Col>
               <h2 className="fs-3">Email Sender</h2>
+              <p className="text-secondary">Untuk mengirim Email, kita memerlukan sebuah protokol yang bernama SMTP. SMTP sendiri sudah built-in dari package net/http Golang. Selain itu kita juga harus memiliki credentialsnya. Berikut adalah sample yang akan dibuat dengan Golang.</p>
             </Col>
           </Row> 
         </Container>
