d9a92ba928c240f78c9609f849d9edc14b484c7e
RageNeko26 <rneko2006@gmail.com>
2023-10-09 11:57:00 +0700

    feat: Add code block

--- src/pages/EmailSender/EmailSender.tsx
+++ src/pages/EmailSender/EmailSender.tsx
@@ -1,14 +1,37 @@
 import { Container, Row, Col } from "react-bootstrap"
+import { CodeBlock } from "react-code-blocks"
+
 
 export default function EmailSender() {
+  const sample: string = `
+    package main
+
+    import "fmt"
+
+    func main() {
+        fmt.Println("Hello world")
+    }
+    `
+  
+
     return(
         <Container className="my-3">
           <Row lg={2}>
             <Col>
               <h2 className="fs-3">Email Sender</h2>
-              <p className="text-secondary">Untuk mengirim Email, kita memerlukan sebuah protokol yang bernama SMTP. SMTP sendiri sudah built-in dari package net/http Golang. Selain itu kita juga harus memiliki credentialsnya. Berikut adalah sample yang akan dibuat dengan Golang.</p>
-            </Col>
-          </Row> 
+              <p className="text-secondary">Untuk mengirim Email, kita memerlukan sebuah protokol yang bernama SMTP. SMTP sendiri sudah built-in dari package net/http Golang. Selain itu kita juga harus memiliki credentialsnya. </p>
+            </Col> 
+          </Row>
+          <Row lg={2}>
+            <Col>
+              <h2 className="fs-3">Basic Usage</h2>
+              <p className="text-secondary">Berikut penggunaan basic untuk mail sender.</p>
+              <CodeBlock text={ sample }
+                          language="go"
+                          showLineNumbers={true}
+                          theme="atom-one-dark" />
+            </Col>
+          </Row>
         </Container>
       )
 }
